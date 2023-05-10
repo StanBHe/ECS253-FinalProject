2692e61e3ef7abaef70fc59f97ccdfcea558bf9c
Stan He <49423142+StanBHe@users.noreply.github.com>
2023-05-09 23:12:10 -0700

    More processing

--- AppendGameInfo.py
+++ AppendGameInfo.py
@@ -3,15 +3,25 @@ import requests
 
 df = pandas.read_csv("applicable_games_appid.csv")
 
+# url = "http://steamspy.com/api.php?request=appdetails&appid={}".format(730)
+# print(url)
+# response = requests.get(url)
+#print(response.json())
 
-url = "http://steamspy.com/api.php?request=appdetails&appid={}".format(df.iloc[1].get("appid"))
-print(url)
-response = requests.get(url)
+data = []
+columns = ['appid', 'name', 'positive','negative','price', 'languages', 'genre', 'tags']
 
-print(response.json())
+dataframe_size = len(df)
+for index, row in df.iterrows():
+    #print(row['appid'], row['name'])
+    url = "http://steamspy.com/api.php?request=appdetails&appid={}".format(row['appid'])
+    response = requests.get(url)
+    response_params = [response.json().get('appid'), response.json().get('name'), response.json().get('positive'), response.json().get('negative'),
+                  response.json().get('price'),response.json().get('languages'),response.json().get('genre'),response.json().get('tags')]
+    #print(response_params)
+    data.append(response_params)
+    print(index/dataframe_size * 100)
 
-# for index, row in df.iterrows():
-#     print(row['appid'], row['name'])
-    
-#     url = "steamspy.com/api.php?request=appdetails&appid={}".format(row['appid'])
-#     response = requests.get(url)
+#print(df['tags'][0])
+df = pandas.DataFrame(data = data, columns = columns)
+df.to_csv('extracted_games_attributes.csv', index=False, encoding='utf-8')
